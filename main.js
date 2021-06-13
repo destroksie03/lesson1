b21efc0f2ab5774fd34f620b85b72fd506f73075
destroksie03 <85576902+destroksie03@users.noreply.github.com>
2021-06-13 17:55:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+let city = 'Barnaul';
+let country = 'Russia';
+let peoples = 600000;
+let stadium = true;
+let about = `${city} ${country} ${peoples} ${stadium}`;
+console.log(about);
+*/
+
+/*
+let height = 40;
+let weight = 70;
+let area = height * weight;
+console.log(area);
+*/
+
+/*
+let time = 2;
+let speedOfFirst = 95;
+let speedOfSecond = 114;
+
+let distance = (speedOfFirst + speedOfSecond) * time;
+console.log(distance);
+*/
+
+/*
+const randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+if (randomNumber < 20) {
+    console.log('randomNumber меньше 20');
+} else if (randomNumber > 50) {
+    console.log('randomNumber больше 50');
+} else {
+    console.log('randomNumber больше 20, и меньше 50');
+}
+*/
+
+/*
+const randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+switch (true) {
+    case randomNumber > 0 && randomNumber <= 20:
+        console.log('randomNumber меньше 20');
+        break;
+    case randomNumber > 50:
+        console.log('randomNumber больше 50');
+        break;
+    default:
+        console.log('randomNumber больше 20, и меньше 50');
+}
+*/
